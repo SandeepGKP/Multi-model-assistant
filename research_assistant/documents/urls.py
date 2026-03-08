376b5d21f55a202c4114cc17266a0dcb68b66c51
SandeepGKP <sandeepnishad638672@gmail.com>
2026-03-08 12:40:19 +0530

    tesseact-ocr modified----5

--- research_assistant/documents/urls.py
+++ research_assistant/documents/urls.py
@@ -2,7 +2,8 @@
 
 from django.urls import path
 from .views import UploadDocumentView
-
+from .test_tesseract_view import test_tesseract_view
 urlpatterns = [
     path("upload/", UploadDocumentView.as_view(), name="upload"),
+    path("tesseract_view/",test_tesseract_view)
 ]
